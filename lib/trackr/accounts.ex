dde7552c83bbd300f0d710c3468628dff81ef67b
Eduardo Cunha <hey@drgmr.com>
2020-03-08 18:08:28 -0300

    Add User schema and migration

--- lib/trackr/accounts.ex
+++ lib/trackr/accounts.ex
@@ -1,6 +1,19 @@
 defmodule Trackr.Accounts do
+  @moduledoc """
+  Provides operations over users and other account resources.
+  """
+  alias Trackr.Repo
+
+  alias Trackr.Accounts.User
+
   @spec get_user(Ecto.UUID.t()) :: {:ok, term()} | {:error, :not_found}
-  def get_user(_id) do
-    {:ok, :user}
+  def get_user(id) do
+    case Repo.get(User, id) do
+      nil ->
+        {:error, :not_found}
+
+      user ->
+        {:ok, user}
+    end
   end
 end
---
+++ test/trackr/accounts_test.exs
@@ -0,0 +1,10 @@
+defmodule Trackr.AccountsTest do
+  use Trackr.DataCase
+
+  alias Trackr.Accounts
+
+  describe "get_user/1" do
+    test "finds an existing user"
+    test "fails if user doesn't exist"
+  end
+end
